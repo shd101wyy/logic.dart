cc3e5f583c52b57485d790d6817975647fa81275
Yiyi Wang <shd101wyy@gmail.com>
2019-05-28 12:22:48 +0800

    Add walk and unify functions

--- lib/src/logic_base.dart
+++ lib/src/logic_base.dart
@@ -1,4 +1,4 @@
-// TODO: Put public facing types in this file.
+import "package:built_collection/built_collection.dart";
 
 /// Checks if you are awesome. Spoiler: you are.
 class Awesome {
@@ -9,6 +9,10 @@ class Awesome {
 class LVar {
   String id;
   LVar(this.id);
+  @override
+  String toString() {
+    return this.id;
+  }
 }
 
 int lvarCounter = 0; // global counter
@@ -20,6 +24,101 @@ LVar lvar(String id) {
   return LVar(id);
 }
 
+int dot() => 0;
 
+Object walk(Object key, BuiltMap<Object, Object> sMap) {
+  if (key is LVar) {
+    if (sMap.containsKey(key)) {
+      var val = sMap[key];
+      return walk(val, sMap); // continue
+    } else {
+      return key; // not found
+    }
+  } else {
+    return key;
+  }
+}
 
+Object deepwalk(Object key, BuiltMap<Object, Object> sMap) {
+  var val = walk(key, sMap);
+  if (val is List) {
+    var o = [];
+    for (var i = 0; i < val.length; i++) {
+      var x = val[i];
+      if (x == dot) {
+        var rest = deepwalk(val[i + 1], sMap);
+        o.addAll(rest);
+        break;
+      } else {
+        o.add(deepwalk(x, sMap));
+      }
+    }
+    return o;
+  } else {
+    return val;
+  }
+}
 
+BuiltMap<Object, Object> unify(
+    Object x, Object y, BuiltMap<Object, Object> sMap) {
+  x = walk(x, sMap);
+  y = walk(y, sMap);
+  if (x == y) {
+    return sMap;
+  } else if (x is LVar) {
+    return sMap.rebuild((m) => m[x] = y);
+  } else if (y is LVar) {
+    return sMap.rebuild((m) => m[y] = x);
+  } else if (x is List && y is List) {
+    return unifyArray(x, y, sMap);
+  } else {
+    // failed to unify
+    return null;
+  }
+}
+
+BuiltMap<Object, Object> unifyArray(
+    List x, List y, BuiltMap<Object, Object> sMap) {
+  if (x.isEmpty && y.isEmpty) {
+    return sMap;
+  }
+  if (x[0] == dot) {
+    return unify(x[1], y, sMap);
+  } else if (y[1] == dot) {
+    return unify(y[1], x, sMap);
+  } else if ((x.isNotEmpty && y.isEmpty) || (x.isEmpty && y.isNotEmpty)) {
+    return null;
+  }
+  var s = unify(x[0], y[0], sMap);
+  if (s != null) {
+    return unify(x.sublist(1), y.sublist(1), s);
+  } else {
+    return s;
+  }
+}
+
+Function succeed() {
+  Iterable<BuiltMap<Object, Object>> succeed_(
+      BuiltMap<Object, Object> sMap) sync* {
+    yield sMap;
+  }
+
+  return succeed_;
+}
+
+Function fail() {
+  Iterable<BuiltMap<Object, Object>> fail_(
+      BuiltMap<Object, Object> sMap) sync* {
+    yield null;
+  }
+
+  return fail_;
+}
+
+Function eq(Object x, Object y) {
+  Iterable<BuiltMap<Object, Object>> eq_(BuiltMap<Object, Object> sMap) sync* {
+    yield unify(x, y, sMap);
+  }
+
+  return eq_;
+}
--- test/logic_test.dart
+++ test/logic_test.dart
@@ -1,3 +1,4 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:logic/logic.dart';
 import 'package:test/test.dart';
 
@@ -11,6 +12,70 @@ void main() {
 
     test('First Test', () {
       expect(awesome.isAwesome, isTrue);
+    });
+  });
+
+  group("Test walk function", () {
+    test('case 1', () {
+      var x = LVar("x");
+      var sMap = BuiltMap<Object, Object>({x: 12});
+      expect(walk(x, sMap), 12);
+    });
+
+    test('case 2', () {
+      var x = LVar("x");
+      var y = LVar("y");
+      var sMap = BuiltMap<Object, Object>({x: y});
+      expect(walk(x, sMap), y);
+    });
+
+    test('case 3', () {
+      var x = LVar("x");
+      var y = LVar("y");
+      var sMap = BuiltMap<Object, Object>({x: y, y: 13});
+      expect(walk(x, sMap), 13);
+    });
+
+    test('case 4', () {
+      var x = LVar("x");
+      var y = LVar("y");
+      var sMap = BuiltMap<Object, Object>({y: 12});
+      expect(walk(x, sMap), x);
+    });
+  });
+
+  group("Test unify function", () {
+    test('case 1', () {
+      var sMap = BuiltMap<Object, Object>();
+      var x = LVar("x");
+      var targetSMap = BuiltMap<Object, Object>({x: 12});
+      expect(unify(x, 12, sMap), targetSMap);
+    });
+
+    test('case 2', () {
+      var x = LVar("x");
+      var y = LVar("y");
+      var sMap = BuiltMap<Object, Object>();
+      var targetSMap = BuiltMap<Object, Object>({x: y});
+      expect(unify(x, y, sMap), targetSMap);
+    });
+
+    test('case 3', () {
+      var x = LVar("x");
+      var m = LVar("m");
+      var n = LVar("n");
+      var sMap = BuiltMap<Object, Object>({x: 12, m: n});
+      var targetSMap = BuiltMap<Object, Object>({x: 12, m: n, n: 12});
+      expect(unify(x, m, sMap), targetSMap);
+    });
+
+    test('case 4', () {
+      var x = LVar("x");
+      var m = LVar("m");
+      var n = LVar("n");
+      var sMap = BuiltMap<Object, Object>({x: 12, m: n});
+      var targetSMap = BuiltMap<Object, Object>({x: 12, m: n, n: 12});
+      expect(unify(m, x, sMap), targetSMap);
     });
   });
 }
